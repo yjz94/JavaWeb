e779c3662aeda1cab51788b456f84769ceced5ea
fishZH <jiangzhong91@gmail.com>
2021-10-27 22:14:17 +0800

    添加获得请求uri结尾值

--- src/main/java/cn/fishland/javaweb/util/FunctionUtils.java
+++ src/main/java/cn/fishland/javaweb/util/FunctionUtils.java
@@ -1,6 +1,9 @@
 package cn.fishland.javaweb.util;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 一些独立功能类
@@ -18,6 +21,42 @@ public class FunctionUtils {
      */
     public static String getUUID() {
         return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /**
+     * 获得str里与pattern匹配的第一个内容
+     *
+     * @param str     原始字符串
+     * @param pattern 匹配规则
+     * @return 符合规则内容，未成功匹配返回null
+     */
+    public static String matchFirstString(String str, String pattern) {
+        try {
+            Pattern compile = Pattern.compile(pattern);
+            Matcher matcher = compile.matcher(str);
+            if (matcher.find()) {
+                return matcher.group(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获得url中最后一个字符串,忽略参数 <br/>
+     * 例：https://github.com/redis/jedis/wiki/Getting
+     * 结果：Getting
+     *
+     * @param request http请求对象
+     * @return 符合条件的字符串
+     */
+    public static String getUriTail(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        if (uri != null) {
+            return uri.replace(request.getContextPath(), "");
+        }
+        return null;
     }
 
 }
