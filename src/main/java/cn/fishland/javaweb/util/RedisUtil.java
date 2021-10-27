937e3572f84d61f9abc8c399d8c8a9ffc55ffc18
fishZH <jiangzhong91@gmail.com>
2021-10-27 22:15:53 +0800

    操作redis工具类

--- src/main/java/cn/fishland/javaweb/util/RedisUtil.java
+++ src/main/java/cn/fishland/javaweb/util/RedisUtil.java
@@ -4,6 +4,8 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.Map;
+
 /**
  * redis相关操作工具类
  *
@@ -13,7 +15,7 @@ import redis.clients.jedis.JedisPoolConfig;
  */
 public class RedisUtil {
 
-    private static JedisPool pool;
+    private static final JedisPool POOL;
 
     static {
         JedisPoolConfig poolConfig = new JedisPoolConfig();
@@ -23,12 +25,12 @@ public class RedisUtil {
         //jedis 第一次启动时，会报错
         poolConfig.setTestOnBorrow(false);
         poolConfig.setTestOnReturn(true);
-        pool = new JedisPool(poolConfig, "127.0.0.1", 6379);
+        POOL = new JedisPool(poolConfig, "127.0.0.1", 6379);
     }
 
     public static Jedis getJedis() {
         try {
-            return pool.getResource();
+            return POOL.getResource();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -72,6 +74,19 @@ public class RedisUtil {
         }
     }
 
+    public static boolean exist(String key) {
+        Jedis jedis = null;
+        try {
+            jedis = getJedis();
+            return jedis.exists(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            close(jedis);
+        }
+        return false;
+    }
+
     public static void setLPush(String key, String... params) {
         Jedis jedis = null;
         try {
@@ -106,6 +121,31 @@ public class RedisUtil {
         } catch (Exception e) {
             e.printStackTrace();
             return "";
+        } finally {
+            close(jedis);
+        }
+    }
+
+    public static String getHash(String key, String filed) {
+        Jedis jedis = null;
+        try {
+            jedis = getJedis();
+            return jedis.hmget(key, filed).get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            close(jedis);
+        }
+    }
+
+    public static void setHash(String key, Map<String, String> map) {
+        Jedis jedis = null;
+        try {
+            jedis = getJedis();
+            jedis.hmset(key, map);
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
             close(jedis);
         }
