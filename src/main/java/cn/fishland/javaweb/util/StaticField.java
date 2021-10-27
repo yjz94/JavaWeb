d3000abb4b28a5500e468a20c63e37badd9d3e50
fishZH <jiangzhong91@gmail.com>
2021-10-27 22:16:12 +0800

    项目通用字段

--- src/main/java/cn/fishland/javaweb/util/StaticField.java
+++ src/main/java/cn/fishland/javaweb/util/StaticField.java
@@ -9,4 +9,16 @@ package cn.fishland.javaweb.util;
  */
 public class StaticField {
 
+    /** 存放草稿文章id的key */
+    public static String ARTICLE_TEMP_UUID_KEY = "articleId";
+    /** 存放草稿文章title的字段 */
+    public static String ARTICLE_TEMP_TITLE_FIELD = "title";
+    /** 存放草稿文章content的字段 */
+    public static String ARTICLE_TEMP_CONTENT_FIELD = "content";
+    /** 存放草稿文章tags的字段 */
+    public static String ARTICLE_TEMP_TAGS_FIELD = "tags";
+
+    /** 未上传文章上传附件的id（这里面存在删除和不用的） */
+    public static String ARTICLE_TEMP_ATTACHMENT_ID_LIST = "articleAttachmentList";
+
 }
